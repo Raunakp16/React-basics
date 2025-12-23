19b9dcf875016ce2a7b9622fe9e91684fe7b7976
Raunak <dhanrajpardesi2@example.com>
2025-12-24 00:11:31 +0530

    Added Navbar component and Cards component using props drilling

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-
+import React from 'react'
+import Nav from './Components/Nav'
+import Card from './Components/Card'
 const App = () => {
   return (
-   <>
-   <h1>Hello ji</h1>
-   <h2>namaste</h2>
-   </>
+    <div>
+      <Nav/>
+       <div className='strip'>
+      <Card user='Raunak' Age={21} img='https://images.unsplash.com/photo-1622519407650-3df9883f76a5?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bW9kZWwlMjBtYW58ZW58MHx8MHx8fDA%3D'/>
+      <Card user='Sunny' Age={18} img='https://plus.unsplash.com/premium_photo-1669850858893-068d0a6bf144?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8bW9kZWwlMjBtYW58ZW58MHx8MHx8fDA%3D'/>
+      <Card user='Jayesh' Age={22} img='https://images.unsplash.com/photo-1619533394727-57d522857f89?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8bW9kZWwlMjBtYW58ZW58MHx8MHx8fDA%3D'/>
+</div>
+
+    </div>
   )
 }
 
 export default App
+
---
+++ src/Components/Nav.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div>
+      <div className="nav">
+        <h1>React Basics </h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta, id?</p>
+      </div>
+    </div>
+  )
+}
+
+export default Nav
